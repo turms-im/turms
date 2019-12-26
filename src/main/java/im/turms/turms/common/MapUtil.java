13ffba8eadb20276eb184e0e71211afb5ea2f58a
JamesChenX <eurekajameschen@gmail.com>
2019-12-26 20:04:36 +0800

    Use collection instead of the exact value as params to CRUD more flexibly in controllers enhancement Pt.1 #148
    Former-commit-id: b4e62c6a768673daa1298793f0f26ae9db1b717d

--- src/main/java/im/turms/turms/common/MapUtil.java
+++ src/main/java/im/turms/turms/common/MapUtil.java
@@ -1,11 +1,17 @@
 package im.turms.turms.common;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.function.Function3;
+
+import java.util.*;
+import java.util.function.Function;
 
 public class MapUtil {
-    private MapUtil() {}
+    private MapUtil() {
+    }
 
     public static <K, V> Map<K, V> deepMerge(Map<K, V> map1, Map<? extends K, ? extends V> map2) {
         for (K key : map2.keySet()) {
@@ -39,5 +45,16 @@ public class MapUtil {
             map.put("value", properties);
             return map;
         }
+    }
+
+    public static Mono<Boolean> fluxMerge(Function<Multimap<Long, Long>, Void> mapFiller, Function3<List<Mono<Boolean>>, Long, Set<Long>, Void> monosFiller) {
+        HashMultimap<Long, Long> multimap = HashMultimap.create();
+        mapFiller.apply(multimap);
+        List<Mono<Boolean>> monos = new ArrayList<>(multimap.keySet().size());
+        for (Long key : multimap.keySet()) {
+            Set<Long> values = multimap.get(key);
+            monosFiller.apply(monos, key, values);
+        }
+        return Flux.merge(monos).all(value -> value);
     }
 }
