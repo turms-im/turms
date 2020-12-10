ddfbb6f4d923bed58b4589e7e45f778c5e678159
JamesChenX <eurekajameschen@gmail.com>
2020-12-10 16:00:47 +0800

    polish the code of business.js for demo

--- turms-client-js/demo/js/business.js
+++ turms-client-js/demo/js/business.js
@@ -14,14 +14,6 @@ function appendContainer(element, text, isError) {
     element.scrollTop = element.scrollHeight;
 }
 
-function appendUserOneContainer(text, isError) {
-    appendContainer(userOneNotificationContainer, text, isError);
-}
-
-function appendUserTwoContainer(text, isError) {
-    appendContainer(userTwoNotificationContainer, text, isError);
-}
-
 function beautify(object) {
     if (object instanceof Error) {
         object = {
@@ -31,114 +23,56 @@ function beautify(object) {
     return JSON.stringify(object, null, "\t");
 }
 
+function setupClient(container, client, userId, password, targetId) {
+    client.driver.onSessionDisconnected = disconnectInfo => {
+        appendContainer(container, `onSessionDisconnected: ${beautify(disconnectInfo)}`);
+    };
+    client.notificationService.onNotification = notification => {
+        appendContainer(container, `onNotification: Receive a notification from other users or server: ${beautify(notification)}`);
+    };
+    client.messageService.onMessage = message => {
+        appendContainer(container, `onMessage: Receive a message from other users or server: ${beautify(message)}`);
+    };
+    client.userService.login(userId, password)
+        .then(() => {
+            appendContainer(container, 'login: User one has logged in');
+            client.messageService.queryPendingMessagesWithTotal(1)
+                .then(messages => appendContainer(container, `Offline messages: ${beautify(messages)}`))
+                .catch(error => {
+                    if (error && error.code === 2001) {
+                        appendContainer(container, 'No offline message');
+                    }
+                });
+            const intervalOne = setInterval(() => {
+                if (client.driver.isConnected()) {
+                    client.messageService.sendMessage(
+                        false,
+                        targetId,
+                        new Date(),
+                        "Hello Turms, My userId is " + userId,
+                        null,
+                        30)
+                        .then(id => appendContainer(container, `message ${id} has been sent`))
+                        .catch(error => appendContainer(container, `failed to send message: ${beautify(error)}`, true));
+                } else {
+                    clearInterval(intervalOne);
+                }
+            }, 2000);
+            client.groupService.createGroup(
+                'Turms Developers Group',
+                'This is a group for the developers who are interested in Turms',
+                'nope')
+                .then(id => appendContainer(container, `group ${id} has been created`))
+                .catch(error => appendContainer(container, `failed to create group: ${beautify(error)}`, true));
+        })
+        .catch(reason => appendContainer(container, `failed to log in ${beautify(reason)}`, true));
+}
+
 function start() {
     const clientUserOne = new TurmsClient('ws://localhost:9510', 30 * 1000);
     const clientUserTwo = new TurmsClient('ws://localhost:9510', 30 * 1000);
     const USER_ONE_ID = '1';
     const USER_TWO_ID = '2';
-    clientUserOne.driver.onSessionDisconnected = disconnectInfo => {
-        appendUserOneContainer(`onSessionDisconnected: ${beautify(disconnectInfo)}`);
-    };
-    clientUserOne.notificationService.onNotification = notification => {
-        appendUserOneContainer('onNotification: Receive a notification from other users or server: ' + beautify(notification));
-    };
-    clientUserOne.messageService.onMessage = message => {
-        appendUserOneContainer('onMessage: Receive a message from other users or server: ' + beautify(message));
-    };
-    clientUserTwo.driver.onClose = closeInfo => {
-        appendUserTwoContainer(`onClose: ${beautify(closeInfo)}`);
-    };
-    clientUserTwo.notificationService.onNotification = (notification) => {
-        appendUserTwoContainer('onNotification: Receive a notification from other users or server: ' + beautify(notification));
-    };
-    clientUserTwo.messageService.onMessage = message => {
-        appendUserTwoContainer('onMessage: Receive a message from other users or server: ' + beautify(message));
-    };
-    return Promise.all([
-        clientUserOne.userService.login(USER_ONE_ID, '123')
-            .then(() => {
-                appendUserOneContainer('login: User one has logged in');
-                return Promise.resolve();
-            })
-            .catch(reason => appendUserOneContainer('Failed to log in ' + beautify(reason), true)),
-        clientUserTwo.userService.login(USER_TWO_ID, '123')
-            .then(() => {
-                appendUserTwoContainer('login: User two has logged in');
-                return Promise.resolve();
-            })
-            .catch(reason => appendUserTwoContainer('Failed to log in ' + beautify(reason), true))
-    ])
-        .then(() => {
-                clientUserOne.messageService.queryPendingMessagesWithTotal(1)
-                    .then(messages => {
-                        appendUserOneContainer('Offline messages: ' + beautify(messages));
-                    })
-                    .catch(error => {
-                        if (error && error.code === 2001) {
-                            appendUserOneContainer('No offline message');
-                        }
-                    });
-                const intervalOne = setInterval(() => {
-                    if (clientUserOne.driver.isConnected()) {
-                        clientUserOne.messageService.sendMessage(
-                            false,
-                            USER_TWO_ID,
-                            new Date(),
-                            "Hello Turms, My userId is " + USER_ONE_ID,
-                            null,
-                            30)
-                            .then(id => {
-                                appendUserOneContainer('message ' + id + ' has been sent');
-                            })
-                            .catch(error => {
-                                appendUserOneContainer('failed to send message: ' + beautify(error), true);
-                            });
-                    } else {
-                        clearInterval(intervalOne);
-                    }
-                }, 2000);
-                clientUserOne.groupService.createGroup(
-                    'Turms Developers Group',
-                    'This is a group for the developers who are interested in Turms',
-                    'nope')
-                    .then(id => {
-                        appendUserOneContainer('group ' + id + ' has been created');
-                    })
-                    .catch(error => {
-                        appendUserOneContainer('failed to create group: ' + beautify(error), true);
-                    });
-                clientUserTwo.messageService.queryPendingMessagesWithTotal(1)
-                    .then(messages => {
-                        appendUserTwoContainer('Offline messages: ' + beautify(messages));
-                    })
-                    .catch(error => {
-                        if (error && error.code === 2001) {
-                            appendUserTwoContainer('No offline message');
-                        }
-                    });
-                const intervalTwo = setInterval(() => {
-                    if (clientUserOne.driver.isConnected()) {
-                        clientUserTwo.messageService.sendMessage(
-                            false,
-                            USER_ONE_ID,
-                            new Date(),
-                            "Hello Community, My userId is " + USER_TWO_ID,
-                            null,
-                            30)
-                            .then(id => {
-                                appendUserTwoContainer('message ' + id + ' has been sent');
-                            })
-                            .catch(error => {
-                                appendUserTwoContainer('failed to send message: ' + beautify(error), true);
-                            });
-                    } else {
-                        clearInterval(intervalTwo);
-                    }
-                }, 2000);
-            }
-        )
-        .catch(reason => {
-            appendUserOneContainer('Failed to log in ' + beautify(reason), true);
-            appendUserTwoContainer('Failed to log in ' + beautify(reason), true);
-        });
+    setupClient(userOneNotificationContainer, clientUserOne, USER_ONE_ID, '123', USER_TWO_ID);
+    setupClient(userTwoNotificationContainer, clientUserTwo, USER_TWO_ID, '123', USER_ONE_ID);
 }
