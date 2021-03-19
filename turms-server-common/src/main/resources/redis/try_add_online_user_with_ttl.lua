010fa408a51583017f0c99217d984b605b95922d
JamesChenX <eurekajameschen@gmail.com>
2021-03-19 17:46:44 +0800

    optimize the logic of try_add_online_user_with_ttl.lua

--- turms-server-common/src/main/resources/redis/try_add_online_user_with_ttl.lua
+++ turms-server-common/src/main/resources/redis/try_add_online_user_with_ttl.lua
@@ -7,10 +7,17 @@ local status = KEYS[5]
 local result = redis.call('hsetnx', user_id, device, node_id)
 
 if result > 0 then
-    if status == nil and redis.call('hget', user_id, 's') == nil then
-        redis.call('hset', user_id, 's', '\x00')
+    local previous_status = redis.call('hget', user_id, 's')
+    if previous_status == nil then
+        if status == nil then
+            redis.call('hset', user_id, 's', '\x00')
+        else
+            redis.call('hset', user_id, 's', status)
+        end
     else
-        redis.call('hset', user_id, 's', status)
+        if status ~= nil and previous_status ~= status then
+            redis.call('hset', user_id, 's', status)
+        end
     end
     redis.call('expire', user_id, ttl)
     return true
