5f4a136340882bb18edebd0379060226501c4460
JamesChenX <eurekajameschen@gmail.com>
2021-05-05 10:58:43 +0800

    Create an independent module turms-server-test for system tests

---
+++ turms-server-test/src/test/java/system/BaseSystemTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 The Turms Project
+ * https://github.com/turms-im/turms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package system;
+
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author James Chen
+ */
+public abstract class BaseSystemTest {
+
+    private static final Duration HTTP_TIMEOUT = Duration.ofSeconds(10);
+
+    protected void assertTurmsGatewayAvailable(String host, int port) {
+        String uri = String.format("http://%s:%d/actuator/health", host, port);
+        String response = HttpClient
+                .create()
+                .get()
+                .uri(uri)
+                .responseContent()
+                .aggregate()
+                .asString()
+                .block(HTTP_TIMEOUT);
+        assertThat(response).contains("\"UP\"");
+    }
+
+    static {
+        System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+    }
+
+}
