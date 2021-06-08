78b80af9734212f9e6224d4eecc0bcd0157ff7cb
JamesChenX <eurekajameschen@gmail.com>
2021-06-08 11:36:57 +0800

    Return error status code for InputFileReader#readFileFromHTMLInputElement

--- turms-client-js/src/util/input-file-reader.ts
+++ turms-client-js/src/util/input-file-reader.ts
@@ -1,14 +1,20 @@
+export enum ErrorCode {
+    ELEMENT_IS_NULL = 1,
+    IS_NOT_FILE_LIST,
+    NO_FILES_SELECTED
+}
+
 export default class InputFileReader {
     private static _reader: FileReader;
 
     static readFileFromHTMLInputElement(element: HTMLInputElement, fileIndex = 0): Promise<string | Uint8Array> {
         // element.files instanceof FileList && element.files.length > 0
-        if (typeof element === 'undefined') {
-            return Promise.reject('element is undefined');
+        if (element == null) {
+            return Promise.reject(ErrorCode.ELEMENT_IS_NULL);
         } else if (!(element.files instanceof FileList)) {
-            return Promise.reject('element.files isn\'t a FileList object');
+            return Promise.reject(ErrorCode.IS_NOT_FILE_LIST);
         } else if (element.files.length === 0) {
-            return Promise.reject('no files selected');
+            return Promise.reject(ErrorCode.NO_FILES_SELECTED);
         }
         return this.readFile(element.files[fileIndex]);
     }
