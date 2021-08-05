6f4597d491cf9d73341dd45b82559b0769c86359
JamesChenX <eurekajameschen@gmail.com>
2021-08-05 18:38:49 +0800

    Suppress Redis/MongoDB connection exceptions if application is closing  #784

--- turms-server-common/src/main/java/im/turms/server/common/context/TurmsApplicationContext.java
+++ turms-server-common/src/main/java/im/turms/server/common/context/TurmsApplicationContext.java
@@ -17,16 +17,20 @@
 
 package im.turms.server.common.context;
 
+import io.lettuce.core.RedisException;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Hooks;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * @author James Chen
@@ -38,6 +42,8 @@ public class TurmsApplicationContext {
 
     private static final String BUILD_INFO_PROPS_PATH = "classpath:META-INF/build-info.properties";
     private static final String DEFAULT_VERSION = "0.0.0";
+
+    private boolean isClosing;
 
     private final boolean isProduction;
     private final boolean isDevOrLocalTest;
@@ -69,6 +75,11 @@ public class TurmsApplicationContext {
                 isProduction ? "production" : "non-production");
 
         setupErrorHandlerContext();
+    }
+
+    @EventListener(classes = ContextClosedEvent.class)
+    public void handleContextClosedEvent() {
+        isClosing = true;
     }
 
     private String getActiveEnvProfile(String[] activeProfiles, List<String>... knownEnvProfiles) {
@@ -118,19 +129,24 @@ public class TurmsApplicationContext {
     }
 
     private void setupErrorHandlerContext() {
-        Hooks.onErrorDropped(throwable -> {
+        Hooks.onErrorDropped(t -> {
             // throwable is always the instance of ErrorCallbackNotImplemented
-            Throwable cause = throwable.getCause();
+            Throwable cause = t.getCause();
             if (isReadFromForciblyClosedConnectionException(cause)) {
-                // Ignore the exception in production env because it should not have side effects
+                // Ignore the exception in production env because it should not have side effects,
                 // and we cannot avoid the exception completely because of its root cause.
-                // Log the exception only in non-production env so we can try to optimize the code of
+                // Log the exception only in non-production env, so we can try to optimize the code of
                 // client to close the connection with a 4-way handshake on the client side
                 if (!this.isProduction) {
-                    log.warn(throwable);
+                    log.warn(t);
                 }
             } else {
-                log.error("Unhandled exception", throwable);
+                if (isClosing) {
+                    if (isRedisConnectionClosedException(t) || isMongoConnectionClosedException(t)) {
+                        return;
+                    }
+                }
+                log.error("Unhandled exception", t);
             }
         });
     }
@@ -147,6 +163,16 @@ public class TurmsApplicationContext {
             }
         }
         return false;
+    }
+
+    private boolean isMongoConnectionClosedException(Throwable throwable) {
+        Throwable cause = throwable.getCause();
+        return cause instanceof IllegalStateException && cause.getMessage().equals("state should be: open");
+    }
+
+    private boolean isRedisConnectionClosedException(Throwable throwable) {
+        Throwable cause = throwable.getCause();
+        return cause instanceof RedisException && cause.getMessage().equals("Connection closed");
     }
 
 }
