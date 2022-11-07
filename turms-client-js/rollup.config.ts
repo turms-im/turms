25c369487359ce34df57d217913dfb1e2345b58a
JamesChenX <eurekajameschen@gmail.com>
2022-11-07 16:17:20 +0800

    Replace `rollup-plugin-terser` with `@rollup/plugin-terser`, and `rollup-plugin-terser` with `@rollup/plugin-typescript` + Convert `rollup.config.js` to `rollup.config.ts` for better maintainability + Upgrade deps of turms-client-js

---
+++ turms-client-js/rollup.config.ts
@@ -0,0 +1,121 @@
+import fs from 'fs';
+import path from 'path';
+import { createRequire } from 'module';
+import { defineConfig, OutputOptions, RollupOptions } from 'rollup';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import json from '@rollup/plugin-json';
+import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { visualizer } from 'rollup-plugin-visualizer';
+import dts from 'rollup-plugin-dts';
+
+// @ts-ignore
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+// --experimental-json-modules
+// import pkg from './package.json' assert { type: 'json' };
+
+const input = 'src/turms-client.ts';
+
+function getConfig({
+    output,
+    useBabel,
+    useTerser
+}: {
+    output: OutputOptions,
+    useBabel?: boolean,
+    useTerser?: boolean
+}): RollupOptions {
+    const ts = typescript();
+    const babelPlugin = useBabel
+        ? babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled',
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        'modules': false,
+                        'useBuiltIns': 'usage',
+                        'corejs': 3
+                    }
+                ],
+                '@babel/preset-typescript'
+            ],
+            extensions: ['js', 'ts']
+        })
+        : null;
+    const terserPlugin = useTerser ? terser() : null;
+    return {
+        input,
+        output: [output],
+        plugins: [
+            json(),
+            resolve({ extensions: ['.js', '.ts'] }),
+            commonjs(),
+            ts,
+            babelPlugin,
+            terserPlugin,
+            filesize(),
+            visualizer({
+                gzipSize: true,
+                filename: `stats/${path.basename(output.file)}.size-analysis.html`
+            })
+        ]
+    };
+}
+
+const config: RollupOptions[] = [
+    // CommonJS for Node
+    getConfig({
+        output: {
+            file: pkg.main,
+            format: 'cjs'
+        }
+    }),
+    // ES module for bundlers
+    getConfig({
+        output: {
+            file: pkg.module,
+            format: 'esm'
+        }
+    }),
+    // IIFE for browsers
+    getConfig({
+        output: {
+            file: 'dist/turms-client.iife.js',
+            format: 'iife',
+            name: 'TurmsClient'
+        },
+        useBabel: true
+    }),
+    getConfig({
+        output: {
+            file: 'dist/turms-client.iife.min.js',
+            format: 'iife',
+            name: 'TurmsClient'
+        },
+        useBabel: true,
+        useTerser: true
+    }),
+    // Bundle definition files
+    {
+        input,
+        output: {
+            file: pkg.types,
+            format: 'esm'
+        },
+        plugins: [dts()]
+    }
+];
+
+if (fs.existsSync('dist')) {
+    fs.rmSync('dist', {
+        recursive: true
+    });
+}
+
+export default defineConfig(config);
