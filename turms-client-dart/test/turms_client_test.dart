f9f1ac92f0ccc7e8155612475198c9218ba134b3
JamesChenX <eurekajameschen@gmail.com>
2022-11-09 11:18:52 +0800

    Add CI for turms-client-dart/js/kotlin/swift to ensure that they can pass compilation to avoid forgetting adding new methods and classes

---
+++ turms-client-dart/test/turms_client_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+import 'package:turms_client_dart/src/turms_client.dart';
+
+void main() {
+  final client = TurmsClient();
+
+  group('Test', () {
+    test('Test', () {});
+  });
+}
