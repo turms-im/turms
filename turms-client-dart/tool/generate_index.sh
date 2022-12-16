35b3c8551c7e41a7e2d767ce2ed3f218e619a1f2
JamesChenX <eurekajameschen@gmail.com>
2022-12-16 12:19:52 +0800

    Improve performance to generate index file for turms-client-dart

--- turms-client-dart/tool/generate_index.sh
+++ turms-client-dart/tool/generate_index.sh
@@ -3,10 +3,10 @@
 BASEDIR="$(dirname "$(dirname "$0")")"
 SRC_DIR="$BASEDIR/lib/src"
 
-CONTENT="library turms_client_dart;\n"
 FILES=$(find "$SRC_DIR" -type f -name "*.dart" -printf "src/%P\n")
 COUNT=$(echo "$FILES" | wc -l)
 INDEX=0
+EXPORTS=()
 for FILE in $FILES; do
     ((INDEX++))
     if echo "$FILE" | grep -q -E '.pbjson.|.pbenum.'; then
@@ -15,7 +15,8 @@ for FILE in $FILES; do
     fi
     echo "($INDEX/$COUNT) included: $FILE"
     SHOW=$(echo "$FILE" | sed "s/\//\n/g" | grep "\.dart" | sed "s/\./\n/g" | head -1 | sed -r 's/(^|_)([a-z])/\U\2/g')
-    CONTENT="${CONTENT}\nexport '$FILE' show $SHOW;"
+    EXPORTS+=("\nexport '$FILE' show $SHOW;")
 done
-echo -e "$CONTENT" > "$BASEDIR/lib/turms_client.dart"
+ALL_EXPORTS=$(IFS=; echo "${EXPORTS[*]}")
+echo -e "library turms_client_dart;\n$ALL_EXPORTS" > "$BASEDIR/lib/turms_client.dart"
 echo "Done"
