6851dc5f8f2de77dce1e6d03bee87037639d518d
JamesChenX <eurekajameschen@gmail.com>
2023-03-03 08:13:19 +0800

    Integrate `algolia` with turms docs for a better reading experience

--- turms-docs/src/.vuepress/config.ts
+++ turms-docs/src/.vuepress/config.ts
@@ -1,19 +1,59 @@
 import { defineUserConfig } from 'vuepress';
 import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance';
-import { searchPlugin } from '@vuepress/plugin-search';
+import { docsearchPlugin } from '@vuepress/plugin-docsearch';
 import { defaultTheme } from '@vuepress/theme-default';
 
 export default defineUserConfig({
     title: 'Turms Documentation',
     base: '/docs/',
     plugins: [
-        searchPlugin({
+        docsearchPlugin({
+            appId: 'WV3XO0QV2E',
+            apiKey: 'f7a7c652875d1f2cc0f0ec52d8f8f660',
+            indexName: 'turms-imio',
             locales: {
                 '/': {
-                    placeholder: '搜索',
+                    placeholder: '搜索文档',
+                    translations: {
+                        button: {
+                            buttonText: '搜索文档',
+                            buttonAriaLabel: '搜索文档'
+                        },
+                        modal: {
+                            searchBox: {
+                                resetButtonTitle: '清除查询条件',
+                                resetButtonAriaLabel: '清除查询条件',
+                                cancelButtonText: '取消',
+                                cancelButtonAriaLabel: '取消'
+                            },
+                            startScreen: {
+                                recentSearchesTitle: '搜索历史',
+                                noRecentSearchesText: '没有搜索历史',
+                                saveRecentSearchButtonTitle: '保存至搜索历史',
+                                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                                favoriteSearchesTitle: '收藏',
+                                removeFavoriteSearchButtonTitle: '从收藏中移除'
+                            },
+                            errorScreen: {
+                                titleText: '无法获取结果',
+                                helpText: '你可能需要检查你的网络连接'
+                            },
+                            footer: {
+                                selectText: '选择',
+                                navigateText: '切换',
+                                closeText: '关闭',
+                                searchByText: '搜索提供者'
+                            },
+                            noResultsScreen: {
+                                noResultsText: '无法找到相关结果',
+                                suggestedQueryText: '你可以尝试查询',
+                                reportMissingResultsLinkText: '点击反馈',
+                                reportMissingResultsText: '你认为该查询应该有结果？'
+                            }
+                        }
+                    }
                 }
-            },
-            maxSuggestions: 10
+            }
         }),
         mdEnhancePlugin({
             codetabs: true,
