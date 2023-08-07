8ad77b8a45ee5e7b2df2d9ebfb55820bdb93ecf0
JamesChenX <eurekajameschen@gmail.com>
2023-08-07 20:08:26 +0800

    Allow explicit `any` in turms-client-js

--- turms-client-js/.eslintrc.js
+++ turms-client-js/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
         '@typescript-eslint/semi': ['error'],
