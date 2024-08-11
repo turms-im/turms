8ae40f182260ccc162b2377fdcb996f9b81720ff
JamesChenX <eurekajameschen@gmail.com>
2024-08-11 09:45:14 +0800

    Fix `lint` command doesn't work well in turms-admin

--- turms-admin/.eslintrc.cjs
+++ turms-admin/.eslintrc.cjs
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
     root: true,
     env: {
--- turms-admin/.stylelintrc.js
+++ turms-admin/.stylelintrc.js
@@ -1,13 +1,16 @@
-module.exports = {
+export default {
     extends: [
         'stylelint-config-standard',
         'stylelint-config-recess-order',
         'stylelint-config-standard-scss',
         'stylelint-config-recommended-vue'
     ],
+    plugins: [
+        "@stylistic/stylelint-plugin"
+    ],
     rules: {
-        indentation: [4],
         // BEM
-        'selector-class-pattern': '^([a-z][a-z0-9]*)|(--|-|__|\.[a-z0-9]+).*$'
+        'selector-class-pattern': '^([a-z][a-z0-9]*)|(--|-|__|\.[a-z0-9]+).*$',
+        '@stylistic/indentation': 4
     }
 };
